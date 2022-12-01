a64fedb4bb53958eed1a706bc531ec4874b375d1
jmedina <ndgns2@gmail.com>
2022-12-01 11:54:57 -0700

    Day 1 Solutions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { elfCalories } from "../src/dataSets";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  //ADVENT CALENDAR
+
+  //DAY 1 PT.1 SOLUTION:
+  const findTotal = (calSet) => {
+    return calSet.reduce((acc, cur) => {
+      acc += cur;
+      return acc;
+    }, 0);
+  };
+  const findMost = (elfCalories) => {
+    let most = 0;
+    elfCalories.forEach((set) => {
+      let subset = findTotal(set);
+      if (subset > most) {
+        most = subset;
+      }
+    });
+    return most;
+  };
+
+  //DAY 1 PT.2 SOLUTION:
+  const findTop3 = (calSet) => {
+    const total = calSet
+      .map((set) => findTotal(set))
+      .sort((a, b) => b - a)
+      .splice(0, 3)
+      .reduce((acc, cur) => (acc += cur));
+
+    return total;
+  };
+
+  const removeDupes = (array) => {
+  const arrayWithDupes = [1, 2, 34, 4, 5, 6, 7, 8, 9, 9, 0, 5, 4, 2];
+    //#1
+    // const del = [... new Set(array)]
+    //#2
+    // const del = array.filter((n, i) => array.indexOf(n) === i);
+    //#3
+    // const del = []
+    // arrayWithDupes.forEach(n => !del.includes(n) && del.push(n))
+    //#4
+    // const del = array.reduce((acc, cur) => {
+    //   !acc.includes(cur) && acc.push(cur);
+    //   return acc;
+    // }, []);
+  };
+  // removeDupes(arrayWithDupes);
+
+
+return (
+  <main>
+    <h1>ANSWER: </h1>
+    <h2>{findTop3(elfCalories)}</h2>
+  </main>
+);
+
+
+};
 
 export default App;
